4965b04e9f8f4aa9115f986f7a514eeaa20e263c
shu <a.0066715@gmail.com>
2025-12-29 08:41:45 +0800

    初始版本

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { zh_TW, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { icons } from '../icon';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+
+registerLocaleData(zh);
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideNzIcons(icons),
+    provideNzI18n(zh_TW),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+  ]
+};
---
+++ src/icon.ts
@@ -0,0 +1,25 @@
+import {
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  FormOutline,
+  MenuOutline,
+  BarChartOutline,
+  ShoppingOutline,
+  SolutionOutline,
+  AuditOutline,
+  PlusOutline,
+  InfoCircleOutline,
+  GoogleOutline,
+  UnorderedListOutline,
+  RedoOutline,
+  MinusOutline,
+  ArrowLeftOutline
+} from '@ant-design/icons-angular/icons';
+
+export const icons = [
+  MenuFoldOutline, MenuUnfoldOutline, FormOutline,
+  ShoppingOutline, MenuOutline, BarChartOutline,
+  SolutionOutline, AuditOutline, PlusOutline,
+  InfoCircleOutline, GoogleOutline, UnorderedListOutline,
+  RedoOutline, MinusOutline, ArrowLeftOutline
+];
