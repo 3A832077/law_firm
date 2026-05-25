64727c01a56eefb863a019e39898d1c2a383da73
陳舒婷 <a.0066715@gmail.com>
2026-05-25 15:50:14 +0800

    把共用樣式寫到style.css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import { zh_TW, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { icons } from '../icon';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
@@ -21,7 +20,6 @@ export const appConfig: ApplicationConfig = {
     provideNzIcons(icons),
     provideNzI18n(zh_TW),
     importProvidersFrom(FormsModule),
-    provideAnimationsAsync(),
     provideHttpClient(withFetch()),
   ]
 };
